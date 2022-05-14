05142ed457dd6d88a7a91ae748ef474875a58da9
brkp <brkp@tutanota.com>
2022-05-15 01:51:08 +0300

    add the visitor methods to the expressions

--- scripts/generate_exprs_file.py
+++ scripts/generate_exprs_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+# TODO: come up with something better
+# when i feel like doing so
+
 FILE_HEADER = '''\
 # coding: utf-8
+
+from abc import ABC, abstractmethod
 
 from typing import Any
 from pox.scanner import Token
@@ -17,18 +22,33 @@ EXPRS_TEMPLATE = '''\
 class {0}(Expr):
     def __init__(self, {1}):
 {2}
+
+    def accept(self, visitor):
+        return visitor.visit_{3}(self)
 \
+'''
+
+VISITOR_TEMPLATE = '''\
+class Visitor(ABC):
+{}\
+'''
+VISIT_TEMPLATE = '''\
+    @abstractmethod
+    def visit_{0}(self, expr):
+        pass
 '''
 
 def generate_expr_class(name, *fields):
     params = ', '.join(PARAM_TEMPLATE.format(*f.split(' ')) for f in fields)
     fields = '\n'.join(ASSGN_TEMPLATE.format(f.split(' ')[1]) for f in fields)
 
-    return EXPRS_TEMPLATE.format(name, params, fields)
+    return EXPRS_TEMPLATE.format(name, params, fields, name.lower())
 
 def generate_exprs_file(expressions):
+    visits = '\n'.join(VISIT_TEMPLATE.format(expr[0].lower()) for expr in expressions)
+
     print(
-        FILE_HEADER + '\n' +
+        FILE_HEADER + '\n' + VISITOR_TEMPLATE.format(visits) + '\n' +
         '\n'.join(generate_expr_class(n, *f) for n, *f in expressions), end='')
 
 def main():
