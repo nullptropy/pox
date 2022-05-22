d03206a9953b28e544d773266c3552ec30652d2e
brkp <brkp@tutanota.com>
2022-05-22 22:15:35 +0300

    simple ast printer for what we have so far

--- pox/__main__.py
+++ pox/__main__.py
@@ -4,14 +4,37 @@ import sys
 import readline as _
 
 from pox.scanner import Scanner
+from pox.parser import Parser, Visitor
+
+class ASTPrinter(Visitor):
+    def visit_binary(self, expr):
+        return f'({expr.op.lexeme} {expr.lt.accept(self)} {expr.rt.accept(self)})'
+
+    def visit_grouping(self, expr):
+        return f'(group {expr.expressions.accept(self)})'
+
+    def visit_literal(self, expr):
+        return str(expr.value) if expr.value else 'nil'
+
+    def visit_unary(self, expr):
+        return f'({expr.op.lexeme} {expr.expression.accept(self)})'
+
+    def print(self, expression):
+        print(expression.accept(self))
 
 class Pox:
     def __init__(self):
+        self.ast_printer = ASTPrinter()
         self.error_occured = False
 
     def run(self, source):
-        print('\n'.join(map(str, self.tokenize(source))))
-        return 65 if self.error_occured else 0
+        tokens = Scanner(source).scan_tokens(self)
+        expression = Parser(tokens).parse(self)
+
+        if self.error_occured:
+            return 65
+
+        return self.ast_printer.print(expression) or 0
 
     def repl(self):
         while True:
@@ -32,9 +55,6 @@ class Pox:
                 return self.run_file(sys.argv[1])
             case _:
                 return print("usage: pox [path]") or 64
-
-    def tokenize(self, source):
-        return Scanner(source).scan_tokens(self)
 
 if __name__ == '__main__':
     exit(Pox().main())
---
+++ pox/parser/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+
+from .parser import Parser
+from .exprs import Expr, Visitor
