d4157dbd4a3bd4d227292dba417178bb4360083d
brkp <brkp@tutanota.com>
2022-05-25 12:50:07 +0300

    hook up the interpreter

--- pox/__main__.py
+++ pox/__main__.py
@@ -3,38 +3,54 @@
 import sys
 import readline as _
 
-from pox.parser import Parser, Visitor, ParseError
+from pox.parser import Parser, ParseError
 from pox.scanner import Scanner, ScannerError
-
-class ASTPrinter(Visitor):
-    def visit_binary_expr(self, expr):
-        return f'({expr.op.lexeme} {expr.lt.accept(self)} {expr.rt.accept(self)})'
-
-    def visit_grouping_expr(self, expr):
-        return f'(group {expr.expression.accept(self)})'
-
-    def visit_literal_expr(self, expr):
-        return str(expr.value) if expr.value else 'nil'
-
-    def visit_unary_expr(self, expr):
-        return f'({expr.op.lexeme} {expr.expression.accept(self)})'
-
-    def print(self, expression):
-        print(expression.accept(self))
+from pox.interpreter import Interpreter, RuntimeError
 
 class Pox:
     def __init__(self):
-        self.ast_printer = ASTPrinter()
         self.error_occured = False
+        self.runtime_error_occured = False
 
-    def run(self, source):
-        tokens = self.tokenize(source)
-        expression = Parser(tokens).parse()
+    def report_error(self, error):
+        print(error)
+        self.error_occured = True
 
-        if self.error_occured:
-            return 65
+    def repl(self):
+        interpreter = Interpreter()
 
-        return self.ast_printer.print(expression) or 0
+        while True:
+            try:
+                self.error_occured = False
+                self.run(input('::: '), interpreter)
+            except (EOFError, KeyboardInterrupt) as _:
+                return 0
+            except Exception as err:
+                print(err)
+
+    def run_file(self, path):
+        return self.run(open(path, 'r').read(), Interpreter())
+
+    def main(self):
+        match len(sys.argv):
+            case 1:
+                return self.repl()
+            case 2:
+                return self.run_file(sys.argv[1])
+            case _:
+                return print("usage: pox [path]") or 64
+
+    def run(self, source, interpreter):
+        expression = Parser(self.tokenize(source)).parse()
+
+        if not self.error_occured:
+            try:
+                return print(interpreter.interpret(expression)) or 0
+            except RuntimeError as err:
+                self.runtime_error_occured = True
+                self.report_error(err)
+
+        return 70 if self.runtime_error_occured else 65
 
     def tokenize(self, source):
         tokens  = []
@@ -58,32 +74,6 @@ class Pox:
             return parser.parse()
         except ParseError as err:
             self.report_error(err)
-
-    def report_error(self, error):
-        print(error)
-        self.error_occured = True
-
-    def repl(self):
-        while True:
-            try:
-                self.error_occured = False
-                self.run(input('::: '))
-            except (EOFError, KeyboardInterrupt) as _:
-                return 0
-            except Exception as err:
-                print(err)
-
-    def run_file(self, path):
-        return self.run(open(path, 'r').read())
-
-    def main(self):
-        match len(sys.argv):
-            case 1:
-                return self.repl()
-            case 2:
-                return self.run_file(sys.argv[1])
-            case _:
-                return print("usage: pox [path]") or 64
 
 if __name__ == '__main__':
     exit(Pox().main())
--- pox/interpreter/interpreter.py
+++ pox/interpreter/interpreter.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from pox.utils import number
+from pox.utils import number, stringify
 from pox.parser import Visitor
 from pox.scanner import TokenType
 
@@ -17,11 +17,14 @@ class Interpreter(Visitor):
     def evaluate(self, expr):
         return expr.accept(self)
 
+    def interpret(self, expression):
+        return stringify(self.evaluate(expression))
+
     def visit_binary_expr(self, expr):
         lt = self.evaluate(expr.lt)
         rt = self.evaluate(expr.rt)
 
-        if expr.operator.type not in [TokenType.EQUAL_EQUAL, TokenType.BANG_EQUAL, TokenType.PLUS]:
+        if expr.op.type not in [TokenType.EQUAL_EQUAL, TokenType.BANG_EQUAL, TokenType.PLUS]:
             check_number_operands(expr.op, lt, rt)
 
         match expr.op.type:
