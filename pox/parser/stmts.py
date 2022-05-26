bec45eaf6308ed63934b515749c3707b007ac61a
brkp <brkp@tutanota.com>
2022-05-26 10:43:08 +0300

    statements

---
+++ pox/parser/stmts.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from abc import ABC, abstractmethod
+
+class Stmt:
+    pass
+
+class StmtVisitor(ABC):
+    @abstractmethod
+    def visit_expression_stmt(self, expr):
+        pass
+
+    @abstractmethod
+    def visit_print_stmt(self, expr):
+        pass
+
+class Expression(Stmt):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def accept(self, visitor):
+        return visitor.visit_expression_stmt(self)
+
+class Print(Stmt):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def accept(self, visitor):
+        return visitor.visit_print_stmt(self)
