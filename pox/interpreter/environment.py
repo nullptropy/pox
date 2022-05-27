b285c53b34eee88ca0cd424712820b54f92bc5d4
brkp <brkp@tutanota.com>
2022-05-27 09:18:26 +0300

    enclosing environments

--- pox/interpreter/environment.py
+++ pox/interpreter/environment.py
@@ -3,8 +3,9 @@
 from pox.error import RuntimeError
 
 class Environment:
-    def __init__(self):
+    def __init__(self, enclosing=None):
         self.values = {}
+        self.enclosing = enclosing
 
     def define(self, name, value): # name: str
         self.values[name] = value
@@ -13,10 +14,16 @@ class Environment:
         if name.lexeme in self.values:
             return self.values[name.lexeme]
 
+        if self.enclosing:
+            return self.enclosing.get(name)
+
         raise RuntimeError(name, f'undefined variable {name.lexeme}')
 
     def assign(self, name, value):
         if name.lexeme in self.values:
             return self.values.update({name.lexeme: value})
+
+        if self.enclosing:
+            return self.assign(name, value)
 
         raise RuntimeError(name, f'undefined variable {name.lexeme}')
