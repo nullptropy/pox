7f2423cb2d872437ad3e85f565074dd68bf22cce
brkp <brkp@tutanota.com>
2022-06-05 16:25:16 +0300

    `get_at` and `assign_at` functions

--- pox/interpreter/environment.py
+++ pox/interpreter/environment.py
@@ -10,6 +10,14 @@ class Environment:
     def define(self, name, value): # name: str
         self.values[name] = value
 
+    def ancestor(self, distance):
+        environment = self
+
+        for _ in range(distance):
+            environment = environment.enclosing
+
+        return environment
+
     def get(self, name): # name: Token
         if name.lexeme in self.values:
             return self.values[name.lexeme]
@@ -19,6 +27,9 @@ class Environment:
 
         raise RuntimeError(name, f'undefined variable {name.lexeme}')
 
+    def get_at(self, distance, name): # name: str
+        return self.ancestor(distance).values[name]
+
     def assign(self, name, value):
         if name.lexeme in self.values:
             return self.values.update({name.lexeme: value})
@@ -27,3 +38,6 @@ class Environment:
             return self.enclosing.assign(name, value)
 
         raise RuntimeError(name, f'undefined variable {name.lexeme}')
+
+    def assign_at(self, distance, name, value):
+        self.ancestor(distance).values.update({name.lexeme: value})
